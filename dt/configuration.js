09ee9b7d1fed787955c0e1fc4b21e993e7345f3a
YIXUAN TANG <1659120060@qq.com>
2026-05-21 01:09:02 +0100

    Initial commit of orders_by_shipper project

---
+++ dt/configuration.js
@@ -0,0 +1,26 @@
+/**
+ * This module was created by the BASEditor
+ */
+sap.ui.define(["sap/ui/integration/Designtime"], function (
+    Designtime
+) {
+    "use strict";
+    return function () {
+        return new Designtime({
+        "form": {
+          "items": {
+            "maxItems": {
+              "manifestpath": "/sap.card/configuration/parameters/maxOrdersShown/value",
+              "type": "integer",
+              "label": "Maximum Items",
+              "translatable": false,
+              "description": "Defines how many items will be displayed at most."
+            }
+          }
+        },
+        "preview": {
+          "modes": "LiveAbstract"
+        }
+      });
+    };
+});
